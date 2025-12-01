ddd41030032638b5de04b4680ec23618ba8a64b4
Joel Maxwell <joel.r.maxwell@gmail.com>
2025-12-01 22:02:08 +1100

    Add markdown preview support

---
+++ nvim/lua/joel/plugins/markdown-preview.lua
@@ -0,0 +1,9 @@
+-- File : markdown-preview.lua
+-- Description : Markdown browser preview
+return {
+  "iamcco/markdown-preview.nvim",
+  build = "cd app && npm install",
+  ft = "markdown",
+  config = function()
+  end,
+}
